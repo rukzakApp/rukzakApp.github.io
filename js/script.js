4cbb41c9f99e23985f89ed8eb78865ccbf897b98
Богдан Климов фронт <king-bogdan@mail.ru>
2020-04-19 00:16:20 +0300

    Пробник сайта без адаптива

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ const sliderOutLeft = (item, index) => {
         item.style.animation = `numbersAnimOutLeft 1s ${index / 30}s forwards`;
         setTimeout(() => {
             item.style.transform = 'translateX(-3000px)';
-        }, 1000);
+        }, 100);
     }
 }
 
@@ -51,7 +51,7 @@ const sliderInRight = (item, index) => {
         item.style.animation = `numbersAnimInRight 1s ${index / 30}s forwards`;
         setTimeout(() => {
             item.style.transform = 'translateX(0)';
-        }, 1000);
+        }, 100);
     }
 }
 
@@ -146,6 +146,41 @@ let itemTwo = document.querySelector('.secondItem');
 let itemThree = document.querySelector('.thirdItem');
 let itemFour = document.querySelector('.fourthItem');
 
+let itemsImages = document.querySelectorAll('.itemsImages img');
+
+let firstTitle = document.querySelector('.firstBenefit .title');
+let secondTitle = document.querySelector('.secondBenefit .title');
+let thirdTitle = document.querySelector('.thirdBenefit .title');
+let fourthTitle = document.querySelector('.fourthBenefit .title');
+
+firstTitle.style.transform = 'translateX(0px)';
+
+const titleOut = (item) => {
+    if (item.style.transform === 'translateX(0px)') {
+        item.style.animation = `numbersAnimOutLeft 1s forwards`;
+        setTimeout(() => {
+            item.style.transform = 'translateX(-3000px)';
+        }, 100);
+    }
+}
+
+const titleIn = (item) => {
+    if (item.style.transform !== 'translateX(0px)') {
+        item.style.animation = `numbersAnimInRight 1s forwards`;
+        setTimeout(() => {
+            item.style.transform = 'translateX(0)';
+        }, 100);
+    }
+}
+
+/*numbersImages.forEach((item, index) => {
+    item.style.transform = 'translateX(0px)'
+    //parseInt(item.style.transform.replace(/[^-\d]/g, ''))
+})*/
+
+firstBenefit.forEach((item, index) => {
+    item.style.transform = 'translateX(0px)';
+})
 
 itemOne.addEventListener('click', () => {
     itemTwo.classList.remove('active')
@@ -153,14 +188,19 @@ itemOne.addEventListener('click', () => {
     itemThree.classList.remove('active')
     itemFour.classList.remove('active')
 
-    numbersImages.forEach((item, index) => {
+    itemsImages.forEach((item, index) => {
         item.style.transform = 'translateX(0px)'
     })
 
-    firstTextLi.forEach((item, index) => sliderInRight(item, index))
-    secondTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    thirdTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    fourthTextLi.forEach((item, index) => sliderOutLeft(item, index))
+    titleIn(firstTitle)
+    titleOut(secondTitle)
+    titleOut(thirdTitle)
+    titleOut(fourthTitle)
+
+    firstBenefit.forEach((item, index) => sliderInRight(item, index))
+    secondBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    thirdBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    fourthBenefit.forEach((item, index) => sliderOutLeft(item, index))
 })
 
 itemTwo.addEventListener('click', () => {
@@ -169,14 +209,19 @@ itemTwo.addEventListener('click', () => {
     itemThree.classList.remove('active')
     itemFour.classList.remove('active')
 
-    numbersImages.forEach((item, index) => {
-        item.style.transform = 'translateX(-548px)'
+    itemsImages.forEach((item, index) => {
+        item.style.transform = 'translateX(-514px)'
     })
 
-    secondTextLi.forEach((item, index) => sliderInRight(item, index))
-    firstTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    thirdTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    fourthTextLi.forEach((item, index) => sliderOutLeft(item, index))
+    titleIn(secondTitle)
+    titleOut(firstTitle)
+    titleOut(thirdTitle)
+    titleOut(fourthTitle)
+
+    secondBenefit.forEach((item, index) => sliderInRight(item, index))
+    firstBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    thirdBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    fourthBenefit.forEach((item, index) => sliderOutLeft(item, index))
 })
 
 itemThree.addEventListener('click', () => {
@@ -185,14 +230,19 @@ itemThree.addEventListener('click', () => {
     itemThree.classList.add('active')
     itemFour.classList.remove('active')
 
-    numbersImages.forEach((item, index) => {
-        item.style.transform = `translateX(${-548 * 2}px)`
+    itemsImages.forEach((item, index) => {
+        item.style.transform = `translateX(${-514 * 2}px)`
     })
 
-    thirdTextLi.forEach((item, index) => sliderInRight(item, index))
-    firstTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    secondTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    fourthTextLi.forEach((item, index) => sliderOutLeft(item, index))
+    titleIn(thirdTitle)
+    titleOut(firstTitle)
+    titleOut(secondTitle)
+    titleOut(fourthTitle)
+
+    thirdBenefit.forEach((item, index) => sliderInRight(item, index))
+    firstBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    secondBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    fourthBenefit.forEach((item, index) => sliderOutLeft(item, index))
 })
 
 itemFour.addEventListener('click', () => {
@@ -201,12 +251,17 @@ itemFour.addEventListener('click', () => {
     itemThree.classList.remove('active')
     itemFour.classList.add('active')
 
-    numbersImages.forEach((item, index) => {
-        item.style.transform = `translateX(${-548 * 3}px)`
+    itemsImages.forEach((item, index) => {
+        item.style.transform = `translateX(${-514 * 3}px)`
     })
 
-    fourthTextLi.forEach((item, index) => sliderInRight(item, index))
-    firstTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    secondTextLi.forEach((item, index) => sliderOutLeft(item, index))
-    thirdTextLi.forEach((item, index) => sliderOutLeft(item, index))
+    titleIn(fourthTitle)
+    titleOut(firstTitle)
+    titleOut(thirdTitle)
+    titleOut(secondTitle)
+
+    fourthBenefit.forEach((item, index) => sliderInRight(item, index))
+    firstBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    secondBenefit.forEach((item, index) => sliderOutLeft(item, index))
+    thirdBenefit.forEach((item, index) => sliderOutLeft(item, index))
 })
