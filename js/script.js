76f4db0c4053e29523ea38b325517b6554a4bf0c
itac-company <60584007+itac-company@users.noreply.github.com>
2020-06-26 18:20:41 +0300

    1.0.3

--- js/script.js
+++ js/script.js
@@ -8,3 +8,35 @@ navBar.addEventListener('click', () => {
     navBarMenu.classList.toggle('navBarP__expand');
 
 })
+
+const burger = document.querySelector('.burger');
+const crossRight = document.querySelector('.crossRight');
+const crossLeft = document.querySelector('.crossLeft');
+const burgerSide = document.querySelectorAll('.burgerSide');
+const mobMenuListItems = document.querySelectorAll('.mobMenuList__item')
+let burgerExpanded = false;
+
+burger.addEventListener('click', () => {
+    crossLeft.classList.toggle('rotateLeft')
+    crossRight.classList.toggle('rotateRight')
+    burgerSide.forEach(e => e.classList.toggle('burgerExpand'))
+
+    burgerExpanded = !burgerExpanded
+
+    if(!burgerExpanded) {
+        mobMenuListItems.forEach(e => e.style.transform = 'translateX(100%)')
+        mobMenuListItems[0].style.transform = 'translateY(74px) translateX(100%)'
+    } else {
+        mobMenuListItems[0].style.transform = 'translateY(74px) translateX(0)'
+    }
+})
+
+const forward = (num) => {
+    mobMenuListItems[num].style.transform = "translateX(0)";
+}
+
+const back = (num) => {
+    mobMenuListItems[num].style.transform = "translateX(100%)";
+}
+
+console.log(mobMenuListItems[1])
