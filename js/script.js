daa42e0cfaa9ece2848af3bf294990b19ef4bd6d
itac-company <60584007+itac-company@users.noreply.github.com>
2020-06-27 20:36:58 +0300

    1.0.6

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ const burger = document.querySelector('.burger');
 const crossRight = document.querySelector('.crossRight');
 const crossLeft = document.querySelector('.crossLeft');
 const burgerSide = document.querySelectorAll('.burgerSide');
+const crosses = document.querySelectorAll('.cross');
+const mobMenuList = document.querySelector('.mobMenuList')
 const mobMenuListItems = document.querySelectorAll('.mobMenuList__item')
 let burgerExpanded = false;
 
@@ -25,17 +27,31 @@ burger.addEventListener('click', () => {
 
     if(!burgerExpanded) {
         mobMenuListItems.forEach(e => e.style.left = '100%')
+        setTimeout(() => mobMenuList.style.zIndex = "-1", 500)
+        
     } else {
-        mobMenuListItems[0].style.left = '0'
+        mobMenuListItems[0].style.left = '0';
+        mobMenuList.style.zIndex = "2";
     }
 })
 
+crosses.forEach(e => e.addEventListener('click', () => {
+    mobMenuListItems.forEach(e => e.style.left = '100%')
+    setTimeout(() => mobMenuList.style.zIndex = "-1", 500)
+    crossLeft.classList.remove('rotateLeft')
+    crossRight.classList.remove('rotateRight')
+    burgerSide.forEach(e => e.classList.remove('burgerExpand'))
+    burgerExpanded = false;
+}))
+
 const forward = (num) => {
     mobMenuListItems[num].style.left = "0";
+    mobMenuList.style.zIndex = "10";
 }
 
 const back = (num) => {
     mobMenuListItems[num].style.left = "100%";
+    setTimeout(() => mobMenuList.style.zIndex = "2", 500)
 }
 
 console.log(mobMenuListItems[1])
